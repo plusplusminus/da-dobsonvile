8a797087719f0bf20ef42097a696fa6793aeba9c
iaanvn <iaan@plusplusminus.co.za>
2018-02-27 09:51:56 +0200

    AsideOverlay blend mode added

--- src/components/Compositions/AsideOverlay.js
+++ src/components/Compositions/AsideOverlay.js
@@ -13,6 +13,7 @@ const styles = {
       marginBottom: spacing.space4,
       backgroundSize: 'cover',
       backgroundPosition: 'center center',
+      backgroundBlendMode: 'overlay',
     },
   },
   bg: {
@@ -88,11 +89,11 @@ const AsideOverlay = (props) => {
     asideOverlay: {
       ...styles.asideOverlay.base,
       ...(mb && styles.mb[mb]),
+      ...(imageUrl && color && styles.color[color].imageUrl),
+      ...(!imageUrl && color && styles.color[color].base),
     },
     bg: {
       ...styles.bg,
-      ...(imageUrl && color && styles.color[color].imageUrl),
-      ...(!imageUrl && color && styles.color[color].base),
     }
   });
 
