8ece824f18296381d34ac458a3b85e9347d5a732
Sergio <sergio@plusplusminus.co.za>
2018-03-16 15:50:58 +0200

    new config

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+var path = require('path');
+module.exports = {
+  entry: './src/components/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    libraryTarget: 'commonjs2' // THIS IS THE MOST IMPORTANT LINE! :mindblow: I wasted more than 2 days until realize this was the line most important in all this guide.
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: path.resolve(__dirname, 'src'),
+        exclude: /(node_modules|bower_components|build)/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  externals: {
+    // Don't bundle react or react-dom
+    aphrodite: {
+      commonjs: "aphrodite",
+      commonjs2: "aphrodite",
+      amd: "Aphrodite",
+      root: "Aphrodite"
+    },
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React"
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM"
+    }
+  }
+};
